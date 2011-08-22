0fbd6c535201d10313392118059a116ff74b297e
Bryn Mathias <brynmathias@gmail.com>
2011-08-22 12:31:12 +0200

    Project for looking at PDF uncertainties in CMSSM and SMS scans

---
+++ plottingstuff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+plottingstuff.py
+
+Created by Bryn Mathias on 2011-08-22.
+Copyright (c) 2011 Imperial College. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
